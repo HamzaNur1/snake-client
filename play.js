ccad7f9eb750d9416f310f04cd9ab783e61215c2
Hamza <Hamzanur2010@hotmail.com>
2021-12-01 23:43:02 +0000

    play.js

--- play.js
+++ play.js
@@ -4,4 +4,24 @@ const connection = require('./client')
 
 
 console.log("Connecting ...");
-connection.connect();
+// setupInput(connection.connect())
+
+
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+setupInput(connection.connect())
+// setupInput()
+
